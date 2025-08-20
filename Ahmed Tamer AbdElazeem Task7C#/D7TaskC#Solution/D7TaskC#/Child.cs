8a46da276282d877decce99f17a92ebb598efe00
Ahmed Tamer Alsharqawy <eng.ahmed.tamer554@gmail.com>
2025-08-21 02:09:43 +0300

    Add files via upload

---
+++ Ahmed Tamer AbdElazeem Task7C#/D7TaskC#Solution/D7TaskC#/Child.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D7TaskC_
+{
+    internal class Child : Parent
+    {
+        #region Propertie
+        public int Z { get; set; }
+        #endregion
+
+        #region Constructor
+        public Child(int _X, int _Y, int _Z) : base(_X, _Y)
+        {
+            Z = _Z;
+        }
+        #endregion
+
+        #region Methods
+
+        //public new int Product()
+        //{
+        //    return base.Product() * Z;
+        //}
+
+        public override int Product()
+        {
+            return base.Product() * Z;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+        #endregion
+
+    }
+}
---
+++ Ahmed Tamer AbdElazeem Task7C#/D7TaskC#Solution/D7TaskC#/Parent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D7TaskC_
+{
+    internal class Parent
+    {
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        #endregion
+
+        #region Constructors
+        public Parent(int _X, int _Y)
+        {
+            X = _X;
+            Y = _Y;
+        }
+        #endregion
+
+        #region Methods
+        public virtual int Product()
+        {
+            return X * Y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+        #endregion
+    }
+}
