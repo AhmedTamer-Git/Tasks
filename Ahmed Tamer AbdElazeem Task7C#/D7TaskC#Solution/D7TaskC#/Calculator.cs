83ad42a4bcdafccfbbf0cf86bd5032a503794479
Ahmed Tamer Alsharqawy <eng.ahmed.tamer554@gmail.com>
2025-08-21 19:50:21 +0300

    Add files via upload

---
+++ Ahmed Tamer AbdElazeem Task7C#/D7TaskC#Solution/D7TaskC#/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D7TaskC_
+{
+    internal class Calculator
+    {
+
+        public int Sum(int a, int b) 
+        {
+            return a + b;
+        }
+        public int Sum(int a, int b, int c )
+        { 
+            return a + b + c;
+        }
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+ 
+    }
+}
