9f2273d0dd8dd9df6f78f676c4e4830c88e5e41a
Ahmed Tamer Alsharqawy <eng.ahmed.tamer554@gmail.com>
2025-08-28 01:40:12 +0300

    Add files via upload

---
+++ Ahmed Tamer AbdElazeem Task10C#/D10TaskC#Solution/D10TaskC#/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10TaskC_
+{
+    internal class Employee
+    {
+
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Salary}";
+        }
+
+    }
+}
---
+++ Ahmed Tamer AbdElazeem Task10C#/D10TaskC#Solution/D10TaskC#/Manager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10TaskC_
+{
+    internal class Manager : Employee, IComparable<Manager>
+    {
+        public int CompareTo(Manager other)
+        {
+            return this.Salary.CompareTo(other.Salary);
+        }
+   
+    }
+}
