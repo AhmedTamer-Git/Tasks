38f631ad6197064a6266b0b28d83812918ed5fc3
Ahmed Tamer Alsharqawy <eng.ahmed.tamer554@gmail.com>
2025-08-28 01:20:39 +0300

    Add files via upload

---
+++ Ahmed Tamer AbdElazeem Task10C#/D10TaskC#Solution/D10TaskC#/SortingTwo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D10TaskC_
+{
+    internal class SortingTwo<T>
+    {
+        public void Sort(T[] array, Comparison<T> comparer)
+        {
+            Array.Sort(array, comparer);
+        }
+    }
+}
