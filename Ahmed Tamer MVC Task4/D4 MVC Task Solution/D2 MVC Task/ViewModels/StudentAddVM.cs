f227810070599b5e6d413f5eaa414e3f40b1b8de
AhmedTamer-Git <eng.ahmed.tamer554@gmail.com>
2025-09-29 11:58:10 +0300

    Ahmed Tamer MVC Task4

---
+++ Ahmed Tamer MVC Task4/D4 MVC Task Solution/D2 MVC Task/ViewModels/StudentAddVM.cs
@@ -0,0 +1,13 @@
+﻿using D2_MVC_Task.Models;
+
+namespace D2_MVC_Task.ViewModels
+{
+    public class StudentAddVM
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DepartmentId { get; set; }
+        public List<Department> DeptList { get; set; }
+    }
+
+}
---
+++ Ahmed Tamer MVC Task4/D4 MVC Task Solution/D2 MVC Task/ViewModels/StudentEditVM.cs
@@ -0,0 +1,15 @@
+﻿using D2_MVC_Task.Models;
+
+namespace D2_MVC_Task.ViewModels
+{
+    public class StudentEditVM
+    {
+        public int Id { get; set; }       // Student Id
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DepartmentId { get; set; } // Selected Department
+
+        // عشان نعرض الأقسام كلها في الـ DropDown
+        public List<Department> DeptList { get; set; }
+    }
+}
