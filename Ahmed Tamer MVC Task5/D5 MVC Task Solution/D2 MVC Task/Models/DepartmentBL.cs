06d93d54d03de9e48e0854eeb7d8a2d79e1bcc61
AhmedTamer-Git <eng.ahmed.tamer554@gmail.com>
2025-10-03 00:04:01 +0300

    Ahmed Tamer MVC Task5

---
+++ Ahmed Tamer MVC Task5/D5 MVC Task Solution/D2 MVC Task/Models/DepartmentBL.cs
@@ -0,0 +1,48 @@
+﻿using D2_MVC_Task.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace D2_MVC_Task.Models
+{
+    public class DepartmentBL
+    {
+        
+           LearningSystemDbContext context = new LearningSystemDbContext();
+
+            // Get all departments 
+            public List<Department> GetAll()
+            {
+                return context.Departments
+                              .Include(d => d.Students)
+                              .ToList();
+            }
+
+            // Get department by Id 
+            public Department GetById(int id)
+            {
+                return context.Departments
+                              .Include(d => d.Students)
+                              .FirstOrDefault(d => d.Id == id);
+            }
+
+            // Add new department
+            public void Add(Department dept)
+            {
+                context.Departments.Add(dept);
+                context.SaveChanges();
+            }
+      
+        // Update department
+        public void Update(Department department)
+            {
+                context.Departments.Update(department);
+                context.SaveChanges();
+            }
+
+        // Delete department
+        public void Delete(Department dept)
+        {
+            context.Departments.Remove(dept);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Ahmed Tamer MVC Task5/D5 MVC Task Solution/D2 MVC Task/Models/StudentBL.cs
@@ -0,0 +1,51 @@
+﻿using D2_MVC_Task.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace D2_MVC_Task.Models
+{
+    public class StudentBL
+    {
+        LearningSystemDbContext context = new LearningSystemDbContext();
+
+        // Get all students مع القسم (Eager Loading)
+        public List<Student> GetAll()
+        {
+            return context.Students
+                          .Include(s => s.Department)
+                          .ToList();
+        }
+
+        // Get one student by Id
+        public Student GetById(int id)
+        {
+            return context.Students
+                          .Include(s => s.Department)
+                          .FirstOrDefault(s => s.Id == id);
+        }
+
+        // Add
+        public void Add(Student student)
+        {
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+
+        // Edit
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        // Delete
+        public void Delete(int id)
+        {
+            var st = context.Students.Find(id);
+            if (st != null)
+            {
+                context.Students.Remove(st);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
